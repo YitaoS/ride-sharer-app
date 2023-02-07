2a3b2d09b9d688b51537f03313fe712d14232735
ys386 <ys386@duke.edu>
2023-02-07 18:51:15 -0500

    finalize

--- docker-deploy/web-app/project1/settings.py
+++ docker-deploy/web-app/project1/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['web','vcm-30927.vm.duke.edu', 'localhost', '127.0.0.1']
 
-
+CSRF_TRUSTED_ORIGINS = ['http://*.vcm-30927.vm.duke.edu:8000','http://*.127.0.0.1:8000'] 
 # Application definition
 
 INSTALLED_APPS = [
